cfdd5619eecfd495802bded736804c4ceb668f86
NadiaBoero <nadiaboero34@gmail.com>
2022-06-13 17:18:14 -0300

    Creacion Repositorio + componentes en Angular

---
+++ PortfolioAP/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
